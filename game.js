fa20d9a8c35adc14aa6cb771ec52081c0f50c480
Harrison <htflynn0@gmail.com>
2025-01-15 09:32:10 +0000

    ADD psudocode for the 3 starting classes

---
+++ game.js
@@ -0,0 +1,24 @@
+//build rock paper scissors game
+//3 classes player, human player, computerplayer
+//create a game class to handle the logic of the game
+
+
+//Pseudocode
+/*
+
+class - player (
+properties: name
+)
+
+class - human player - inherits from player(
+methods - choose()
+)
+
+class- computerplayer - inherits from player
+methods - choose()
+
+
+
+
+
+*/
